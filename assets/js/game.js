458f8389ea8cd2fa95c62169416e446af4451b8b
Itzamary Jimenez <itzamary1gonzalez@hotmail.com>
2022-03-08 15:38:33 -0600

    added player health varaiable and expressions and loged them to the console.

--- assets/js/game.js
+++ assets/js/game.js
@@ -25,8 +25,10 @@ function fight() {
     console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
 
     // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+    playerHealth = playerHealth - enemyAttack;
     
     // Log a resulting message to the console so we know that it worked.
+    console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " heath remianing.");
 };
 
 fight();
