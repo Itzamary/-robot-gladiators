6ec4e35f65bdf702351a1b2e16387406d1f20b18
Itzamary Jimenez <itzamary1gonzalez@hotmail.com>
2022-03-08 15:25:51 -0600

    added comments and variables to the js file funcion expressoin on line 18.

--- assets/js/game.js
+++ assets/js/game.js
@@ -16,7 +16,17 @@ console.log(enemyName, enemyHealth, enemyAttack);
 
 // this is a function declaration named fight.
 function fight() {
-    window.alert("The fight has begun");
+    window.alert("Welome to Robot Gladiators!");
+
+    //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+    enemyHealth = enemyHealth - playerAttack;
+
+    // Log a resulting message to the console so we know that it worked.
+    console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + "health remaining.");
+
+    // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+    
+    // Log a resulting message to the console so we know that it worked.
 };
 
 fight();
