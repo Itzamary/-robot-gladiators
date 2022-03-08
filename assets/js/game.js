b53ae170308a8fa70f0af245da59b4c429662931
Itzamary Jimenez <itzamary1gonzalez@hotmail.com>
2022-03-08 15:00:40 -0600

    added recomended variables, comments and functions to js file.

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,22 @@
-window.alert("this is a alert! JavaScript is running");
+// playername info variable declarations.
+var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+
+// log  the following varaiable values to the console using the console.log function.
+console.log(playerName, playerHealth, playerAttack);
+
+// enemy info variable declarations.
+var enemyName = "Roboto"
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+// log the enemy values to the console.
+console.log(enemyName, enemyHealth, enemyAttack);
+
+// this is a function declaration named fight.
+function fight() {
+    window.alert("The fight has begun");
+};
+
+fight();
